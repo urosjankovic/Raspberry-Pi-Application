9e65923ff41eace007117d5681085a8203fe6cc7
UrosJankovic <39414114+UrosJankovic@users.noreply.github.com>
2020-06-10 17:42:10 +0200

    Add files via upload

---
+++ server/serverscript.php
@@ -0,0 +1,11 @@
+<?php
+	if(isset($_POST['filename']))
+	{
+		$jsondata=$_POST['filename'];
+		$content = @file_get_contents($jsondata . ".json");
+		echo $content;
+	}
+	else
+	{
+		echo '{"data":null}';
+	}
