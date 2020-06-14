62cf9b710539d6e8f0f73246698b94ae9ad5700c
UrosJankovic <39414114+UrosJankovic@users.noreply.github.com>
2020-06-14 22:55:58 +0200

    Trying hard

---
+++ Desktop Application/DeskopApp/ViewModels/ConfigViewModel.cs
@@ -0,0 +1,157 @@
+﻿#define CLIENT
+#define GET
+#define DYNAMIC
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Net.Http;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RpiApp.ViewModels
+{
+    using RpiApp.Properties;
+    using Model;
+    using System.Windows.Controls;
+    using RpiApp.ViewModel;
+    using System.Net.Http.Headers;
+    using RpiApp.Views;
+
+    public class ConfigViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+        private string ipAddress;
+
+        public string IpAddress
+        {
+            get
+            {
+                return ipAddress;
+            }
+            set
+            {
+                if (ipAddress != value)
+                {
+                    ipAddress = value;
+                    OnPropertyChanged("IpAddress");
+                }
+            }
+        }
+        private int sampleTime;
+        public string SampleTime
+        {
+            get
+            {
+                return sampleTime.ToString();
+            }
+            set
+            {
+                if (Int32.TryParse(value, out int st))
+                {
+                    if (sampleTime != st)
+                    {
+                        sampleTime = st;
+                        OnPropertyChanged("SampleTime");
+                    }
+                }
+            }
+        }
+
+        private int maxSampleNumber;
+        public string MaxSampleNumber
+        {
+            get
+            {
+                return maxSampleNumber.ToString();
+            }
+            set
+            {
+                if (Int32.TryParse(value, out int msn))
+                {
+                    if (maxSampleNumber != msn)
+                    {
+                        maxSampleNumber = msn;
+                        OnPropertyChanged("MaxSampleNumber");
+                    }
+                }
+            }
+        }
+
+        public ButtonCommand UpdateConfigButton { get; set; }
+        public ButtonCommand DefaultConfigButton { get; set; }
+        #endregion
+
+        #region Fields
+        private ConfigParams config = new ConfigParams();
+        private IoTServer Server;
+        #endregion
+
+        public ConfigViewModel()
+        {
+            UpdateConfigButton = new ButtonCommand(UpdateConfig);
+            DefaultConfigButton = new ButtonCommand(DefaultConfig);
+            
+            ipAddress = config.IpAddress;
+            sampleTime = config.SampleTime;
+            maxSampleNumber = config.MaxSampleNumber;
+
+            Server = new IoTServer(IpAddress);
+        }
+
+        /**
+          * @brief Configuration parameters update
+          */
+        #region ButtonCommands
+        private void UpdateConfig()
+        {
+
+            config = new ConfigParams(ipAddress, sampleTime, maxSampleNumber);
+            Server = new IoTServer(IpAddress);
+
+        }
+
+
+        /**
+          * @brief Configuration parameters defualt values
+          */
+        private void DefaultConfig()
+        {
+
+            config = new ConfigParams();
+            IpAddress = config.IpAddress;
+            SampleTime = config.SampleTime.ToString();
+            MaxSampleNumber = config.MaxSampleNumber.ToString();
+            Server = new IoTServer(IpAddress);
+
+        }
+        #endregion
+
+
+
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /**
+         * @brief Simple function to trigger event handler
+         * @params propertyName Name of ViewModel property as string
+         */
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ Desktop Application/DeskopApp/Views/EnvVieww.xaml.cs
@@ -0,0 +1,70 @@
+﻿using RpiApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Diagnostics;
+
+
+namespace RpiApp.Views
+{
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using RpiApp.Model;
+    using RpiApp.ViewModel;
+    using RpiApp.ViewModels;
+    using System.Security.Cryptography.X509Certificates;
+
+    /// <summary>
+    /// Interaction logic for EnvVieww.xaml
+    /// </summary>
+    public partial class EnvVieww : UserControl
+    {
+        public EnvVieww()
+        {
+            InitializeComponent();
+        }
+
+        public void EnvTable()
+        {
+            DataTable dt = new DataTable();
+            DataColumn tempp = new DataColumn("Temperature", typeof(string));
+            DataColumn presss = new DataColumn("Pressure", typeof(string));
+            DataColumn humidd = new DataColumn("Humidity", typeof(string));
+
+            string url = "http://192.168.1.26/web_app/measurements/tempValues.json";
+         
+            dt.Columns.Add(tempp);
+            dt.Columns.Add(presss);
+            dt.Columns.Add(humidd);
+
+            DataRow firstRow = dt.NewRow();
+            firstRow[0] = "*C";
+            firstRow[1] = "mmHg";
+            firstRow[2] = "%";
+
+            dt.Rows.Add(firstRow);
+
+            DataGridEnv.ItemsSource = dt.DefaultView;
+        }
+
+        private void DataGridEnv_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.EnvTable();
+        }
+    }
+}
