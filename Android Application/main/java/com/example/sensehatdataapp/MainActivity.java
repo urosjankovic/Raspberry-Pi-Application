c7ac92c8b7e6a6bc26ad2cf9b39229b83dc0635b
St3phanie3 <56824290+St3phanie3@users.noreply.github.com>
2020-06-17 08:19:46 +0200

    Update MainActivity.java

--- Android Application/main/java/com/example/sensehatdataapp/MainActivity.java
+++ Android Application/main/java/com/example/sensehatdataapp/MainActivity.java
@@ -29,42 +29,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-       /* Humiditycb=(CheckBox) findViewById(R.id.Humiditycb);
-        tempcb=(CheckBox) findViewById(R.id.tempcb);
-        pressurecb=(CheckBox) findViewById(R.id.pressurecb);
-
-
-
-        Button GoBtn= (Button) findViewById(R.id.GoBtn);
-        GoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //read value from checkboxes
-                int val1=0, val2=0, val3=0;
-
-                if(Humiditycb.isChecked()){
-                    val1=1;
-                }
-
-                if(tempcb.isChecked()){
-                    val2=1;
-                }
-
-                if(pressurecb.isChecked()){
-                    val3=1;
-                }
-                Intent secondIntent= new Intent(getApplicationContext(),ShowVar.class);
-                Bundle checkedval = new Bundle();
-                checkedval.putInt("val1", val1);
-                checkedval.putInt("val2", val2);
-                checkedval.putInt("val3", val3);
-                secondIntent.putExtras(checkedval);
-
-                startActivity(secondIntent);
-
-            }
-        });
-*/
+     
 
         Button LEDBtn= (Button) findViewById(R.id.LEDBtn);
         LEDBtn.setOnClickListener(new View.OnClickListener() {
