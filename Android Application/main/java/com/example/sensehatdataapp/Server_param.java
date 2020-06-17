6874337517799878220b26850568316d2a1a255f
St3phanie3 <56824290+St3phanie3@users.noreply.github.com>
2020-06-17 18:29:29 +0200

    Add files via upload

---
+++ Android Application/main/java/com/example/sensehatdataapp/Server_param.java
@@ -0,0 +1,14 @@
+package com.example.sensehatdataapp;
+
+public class Server_param {
+
+
+    public static String defaultipAddress ="192.168.0.29";
+    public static int defaultsampleTime = 500;
+    public static int defaultsamples = 1000;
+
+    public static String globalip=defaultipAddress;
+    public static int globalsamples=defaultsamples;
+    public static int globalsampletime=defaultsampleTime;
+
+}
