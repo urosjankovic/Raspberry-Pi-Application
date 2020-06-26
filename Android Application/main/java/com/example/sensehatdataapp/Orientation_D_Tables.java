951d56a4329a42180851df4441546debdc90ad7b
St3phanie3 <56824290+St3phanie3@users.noreply.github.com>
2020-06-26 10:15:04 +0200

    Update Orientation_D_Tables.java

--- Android Application/main/java/com/example/sensehatdataapp/Orientation_D_Tables.java
+++ Android Application/main/java/com/example/sensehatdataapp/Orientation_D_Tables.java
@@ -30,7 +30,7 @@ public class Orientation_D_Tables extends AppCompatActivity {
         int val3=extras.getInt("val3A");
 
         if(val1==1){
-            //display data -read the data from json array? with units and variable name
+            //display data 
 
         }
 
